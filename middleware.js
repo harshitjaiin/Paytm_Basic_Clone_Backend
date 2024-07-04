a2aab3f9fba6570643aed521b1bcfabd055fd2ca
harshitjaiin <harshitjain.tj@gmail.com>
2024-07-04 19:49:49 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 const { JWT_SECRET } = require("./config");
-const jwt = require("jsonwebtoken");
-
+const {verify} = require("jsonwebtoken");
+console.log("auth called");
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
@@ -11,7 +11,7 @@ const authMiddleware = (req, res, next) => {
     const token = authHeader.split(' ')[1];
 
     try {
-        const decoded = jwt.verify(token, JWT_SECRET);
+        const decoded = verify(token, JWT_SECRET);
 
         req.userId = decoded.userId;
 
